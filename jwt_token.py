73dc244687575f9a1cb2f45ae84581726819984f
VICTOR MARTINEZ <vmartineza33@gmail.com>
2024-10-27 22:12:47 +0100

    Custom 401 error message

--- jwt_token.py
+++ jwt_token.py
@@ -1,4 +1,5 @@
 """ JWT configuration """
+from flask import make_response
 from flask_jwt_extended import JWTManager
 
 jwt = JWTManager()
@@ -12,3 +13,9 @@ def check_if_token_in_blocklist(jwt_header, jwt_payload):
     """ Check if token in blocklist """
     jti = jwt_payload['jti']
     return jti in blocklist
+
+
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    """ Response returned if accessed without authentication """
+    return make_response("Access denied", 401)
