7e57e00094999c6d39af95db483ad29b487c4571
VICTOR MARTINEZ <vmartineza33@gmail.com>
2024-10-28 00:11:14 +0100

    Try with first 6 characters of uuid

--- services/user_service.py
+++ services/user_service.py
@@ -6,7 +6,7 @@ from models import User, db
 
 def create_user(name, email, password, address, phone_number):
     """ Create new user """
-    account_number = str(uuid.uuid4())
+    account_number = str(uuid.uuid4())[:6]
     new_user = User(name=name, email=email, address=address, phoneNumber=phone_number, accountNumber=account_number)
     new_user.set_password(password)
 
