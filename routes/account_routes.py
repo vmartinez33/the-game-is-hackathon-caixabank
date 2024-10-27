e7516ff1097db4de7dceeae31bbbc5ba7624e451
VICTOR MARTINEZ <vmartineza33@gmail.com>
2024-10-28 00:35:02 +0100

    Created transactions endpoints

--- routes/account_routes.py
+++ routes/account_routes.py
@@ -1,9 +1,9 @@
 """ /api/account routes """
+from datetime import datetime, timezone
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from sqlalchemy.exc import IntegrityError
 
-from models import User, db
+from models import Transaction, User, db
 
 account_bp = Blueprint('account', __name__)
 
@@ -56,3 +56,120 @@ def update_pin():
     db.session.commit()
 
     return jsonify({"msg": "PIN updated successfully"}), 200
+
+
+@account_bp.route('/deposit', methods=['POST'])
+@jwt_required()
+def deposit_money():
+    """ Deposits a specific amount into the user's account """
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    data = request.json
+    pin = data.get("pin")
+    amount = float(data.get("amount"))
+
+    if user.pin != pin:
+        return jsonify("Invalid PIN"), 403
+
+    user.balance += amount
+    db.session.commit()
+
+    transaction = Transaction(amount=amount, transaction_type="CASH_DEPOSIT",
+                              transaction_date=datetime.now(timezone.utc),
+                              source_account_number=user.accountNumber,
+                              target_account_number="N/A")
+    db.session.add(transaction)
+    db.session.commit()
+
+    return jsonify({"msg": "Cash deposited successfully"}), 200
+
+
+@account_bp.route('/withdraw', methods=['POST'])
+@jwt_required()
+def withdraw_money():
+    """ Withdraws a specific amount from the user's account """
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    data = request.json
+    amount = float(data.get("amount"))
+    pin = data.get("pin")
+
+    if user.pin != pin:
+        return jsonify("Invalid PIN"), 403
+
+    if user.balance < amount:
+        return jsonify("Insufficient balance"), 400
+
+    user.balance -= amount
+    db.session.commit()
+
+    transaction = Transaction(amount=amount, transaction_type="CASH_WITHDRAWAL",
+                              transaction_date=datetime.now(timezone.utc),
+                              source_account_number=user.accountNumber,
+                              target_account_number="N/A")
+    db.session.add(transaction)
+    db.session.commit()
+
+    return jsonify({"msg": "Cash withdrawn successfully"}), 200
+
+
+@account_bp.route('/fund-transfer', methods=['POST'])
+@jwt_required()
+def fund_transfer():
+    """ Transfers funds to another account """
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    data = request.json
+    amount = float(data.get("amount"))
+    pin = data.get("pin")
+    target_account_number = data.get("targetAccountNumber")
+
+    if user.pin != pin:
+        return jsonify("Invalid PIN"), 403
+
+    if user.balance < amount:
+        return jsonify("Insufficient balance"), 400
+
+    target_user = User.query.filter_by(accountNumber=target_account_number).first()
+    if not target_user:
+        return jsonify({"msg": "Target account not found"}), 404
+
+    user.balance -= amount
+    target_user.balance += amount
+    db.session.commit()
+
+    transaction = Transaction(amount=amount, transaction_type="CASH_TRANSFER",
+                              transaction_date=datetime.now(timezone.utc),
+                              source_account_number=user.accountNumber,
+                              target_account_number=target_account_number)
+    db.session.add(transaction)
+    db.session.commit()
+
+    return jsonify({"msg": "Fund transferred successfully"}), 200
+
+
+@account_bp.route('/transactions', methods=['GET'])
+@jwt_required()
+def get_transaction_history():
+    """ Retrieves the user's transaction history """
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    transactions = Transaction.query.filter(
+        (Transaction.source_account_number == user.accountNumber) |
+        (Transaction.target_account_number == user.accountNumber)
+    ).all()
+
+    transaction_list = [{
+        "id": transaction.id,
+        "amount": transaction.amount,
+        "transactionType": transaction.transaction_type,
+        "transactionDate": int(transaction.transaction_date.timestamp() * 1000),
+        "sourceAccountNumber": transaction.source_account_number,
+        "targetAccountNumber": transaction.target_account_number
+    } for transaction in transactions]
+
+    return jsonify(transaction_list), 200
